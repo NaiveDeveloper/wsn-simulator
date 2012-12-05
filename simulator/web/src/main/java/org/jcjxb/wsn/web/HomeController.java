b9972179b5a13a85044fa6b6fc265a288b4677d1
ykl <ykl@ykl-vostro-200.(none)>
2012-12-05 21:09:54 +0800

    make web project run and add *.jspt support

---
+++ simulator/web/src/main/java/org/jcjxb/wsn/web/HomeController.java
@@ -0,0 +1,33 @@
+package org.jcjxb.wsn.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/{path:.*}.jspt", method = RequestMethod.GET)
+	public String jsp(@PathVariable("path") String path) {
+		return path;
+	}
+}
